5b81319f219a9a3fcd32561a06eda7ae5f94ba71
elitevitek <42992972+elitevitek@users.noreply.github.com>
2018-10-17 11:28:36 +0300

    Add files via upload

---
+++ TEST3_17_10.py
@@ -0,0 +1,10 @@
+#A Simple Definite Loop
+for i in[5, 4, 3, 2, 1]:
+    print(i)
+print('Blastoff!')
+
+
+frinds = ['Viktor','Kirill','Karen']
+for friend in friends :
+    print('Happy New Year:', friend)
+print('done')
---
+++ test_2.py
@@ -0,0 +1,7 @@
+n = 5
+while n > 0:
+    print('lather')
+    print('Rinse')
+print('Drt off')
+
+ 
